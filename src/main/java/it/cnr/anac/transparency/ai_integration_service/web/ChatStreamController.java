64e4f7db383003b82ebecaf8bb52448f82fc61ce
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2025-12-04 11:48:42 +0100

    Introdotta generazione immagine docker e build github.

--- src/main/java/it/cnr/anac/transparency/ai_integration_service/web/ChatStreamController.java
+++ src/main/java/it/cnr/anac/transparency/ai_integration_service/web/ChatStreamController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2025 Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package it.cnr.anac.transparency.ai_integration_service.web;
 
 import org.slf4j.Logger;
@@ -15,7 +31,6 @@ import org.springframework.http.HttpStatus;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Controller REST che espone un endpoint SSE per lo streaming dei token
